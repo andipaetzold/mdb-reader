27069a0aa18678d9e31cbac5df22acad7a19933f
Andi Pätzold <github@andipaetzold.com>
2020-07-09 23:40:19 +0200

    feat: prepare typescript and rollup

---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+import typescript from "rollup-plugin-typescript2";
+import pkg from "./package.json";
+import { terser } from "rollup-plugin-terser";
+
+export default {
+    input: "src/index.ts",
+    output: [
+        {
+            file: pkg.main,
+            format: "cjs",
+        },
+        {
+            file: pkg.module,
+            format: "es",
+        },
+    ],
+    plugins: [
+        typescript({
+            typescript: require("typescript"),
+        }),
+        terser(),
+    ],
+};
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import MDBReader from "./MDBReader";
+
+export default MDBReader;
