5d753e69c0e462f77925165ef1d4982c0a289863
Andi Pätzold <github@andipaetzold.com>
2021-08-15 15:14:21 +0200

    fix: error message when page has unexpected type

--- src/PageType.ts
+++ src/PageType.ts
@@ -14,6 +14,6 @@ export default PageType;
 
 export function assertPageType(buffer: Buffer, pageType: PageType): void {
     if (buffer[0] !== pageType) {
-        throw new Error(`Wrong page type. Expected ${buffer[0]} but received ${pageType}`);
+        throw new Error(`Wrong page type. Expected ${pageType} but received ${buffer[0]}.`);
     }
 }
