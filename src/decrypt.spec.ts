1f120654396ed42798e684549d96748de1ad61d7
Andi Pätzold <github@andipaetzold.com>
2021-08-15 23:17:29 +0200

    feat: implement rc4 decryption

---
+++ src/decrypt.spec.ts
@@ -0,0 +1,14 @@
+import { decrypt } from "./decrypt";
+
+describe("decrypt", () => {
+    // https://en.wikipedia.org/wiki/RC4#Test_vectors
+    it.each`
+        key         | encrypted                                                                                            | expected
+        ${"Key"}    | ${Buffer.from([0xbb, 0xf3, 0x16, 0xe8, 0xd9, 0x40, 0xaf, 0x0a, 0xd3])}                               | ${"Plaintext"}
+        ${"Wiki"}   | ${Buffer.from([0x10, 0x21, 0xbf, 0x04, 0x20])}                                                       | ${"pedia"}
+        ${"Secret"} | ${Buffer.from([0x45, 0xa0, 0x1f, 0x64, 0x5f, 0xc3, 0x5b, 0x38, 0x35, 0x52, 0x54, 0x4b, 0x9b, 0xf5])} | ${"Attack at dawn"}
+    `("$key", ({ key, encrypted, expected }) => {
+        const result = decrypt(encrypted, Buffer.from(key, "ascii"));
+        expect(result.toString("ascii")).toBe(expected);
+    });
+});
---
+++ src/decrypt.ts
@@ -0,0 +1,40 @@
+export function decrypt(input: Buffer, key: Buffer): Buffer {
+    const keyStream = createKeyStream(key);
+    return decryptBuffer(input, keyStream);
+}
+
+/**
+ * Key-scheduling algorithm (KSA)
+ */
+function createKeyStream(key: Buffer): Uint8Array {
+    const S = new Uint8Array(256);
+    for (let i = 0; i < 256; ++i) {
+        S[i] = i;
+    }
+    let j = 0;
+    for (let i = 0; i < 256; ++i) {
+        j = (j + S[i] + key[i % key.length]) % 256;
+        [S[i], S[j]] = [S[j], S[i]];
+    }
+
+    return S;
+}
+
+/**
+ * Pseudo-random generation algorithm (PRGA)
+ */
+function decryptBuffer(buffer: Buffer, S: Uint8Array): Buffer {
+    const resultBuffer = Buffer.from(buffer);
+
+    let i = 0;
+    let j = 0;
+
+    for (let k = 0; k < buffer.length; ++k) {
+        i = (i + 1) % 256;
+        j = (j + S[i]) % 256;
+        [S[i], S[j]] = [S[j], S[i]];
+        resultBuffer[k] ^= S[(S[i] + S[j]) % 256];
+    }
+
+    return resultBuffer;
+}
