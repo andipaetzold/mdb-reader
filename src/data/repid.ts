c51d90e8dc546e40cb8783a5df80e5461a3276aa
Andi Pätzold <github@andipaetzold.com>
2021-10-22 13:19:14 +0200

    chore: format jsdoc

--- src/data/repid.ts
+++ src/data/repid.ts
@@ -1,7 +1,9 @@
-/** Read replication ID
+/**
+ * Read replication ID
  *
  * @see https://github.com/mdbtools/mdbtools/blob/c3df30837ec2439d18c5515906072dc3306c0795/src/libmdb/data.c#L958-L972
- */ export function readRepID(buffer: Buffer): string {
+ */
+export function readRepID(buffer: Buffer): string {
     return (
         buffer.slice(0, 4).swap32().toString("hex") + // swap for little-endian
         "-" +
