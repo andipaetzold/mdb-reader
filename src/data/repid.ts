d3a816a0d55d6b624d9a08c4d89fe62452979e7e
Redmer Kronemeijer <12477216+redmer@users.noreply.github.com>
2021-10-22 11:50:46 +0200

    fix: mixed-endianness of guids

--- src/data/repid.ts
+++ src/data/repid.ts
@@ -1,4 +1,30 @@
-export function readRepID(buffer: Buffer): string {
+/** Read replication ID
+ * 
+ * @see https://github.com/mdbtools/mdbtools/blob/c3df30837ec2439d18c5515906072dc3306c0795/src/libmdb/data.c#L958-L972
+ */ export function readRepID(buffer: Buffer): string {
     const str = buffer.toString("hex");
-    return `${str.slice(0, 8)}-${str.slice(8, 12)}-${str.slice(12, 16)}-${str.slice(16, 20)}-${str.slice(20)}`;
+    const bytes = str.match(/.{1,2}/g);
+
+    return (
+        bytes[3] + // little-endian
+        bytes[2] +
+        bytes[1] +
+        bytes[0] +
+        "-" +
+        bytes[5] + // little-endian
+        bytes[4] +
+        "-" +
+        bytes[7] + // little-endian
+        bytes[6] +
+        "-" +
+        bytes[8] + // big-endian
+        bytes[9] +
+        "-" +
+        bytes[10] + // big-endian
+        bytes[11] +
+        bytes[12] +
+        bytes[13] +
+        bytes[14] +
+        bytes[15]
+    );
 }
