b1b4379b830041c6a725b67d165d0811e98d8a67
andreasrueedlinger <andreas.rueedlinger@gmail.com>
2021-11-06 00:07:18 +0100

    fix: round milliseconds in datetime parser (#123)
    * round milliseconds in datetime parser
    
    * test: add tests for readDateTime
    
    Co-authored-by: Andi Pätzold <github@andipaetzold.com>

--- src/data/datetime.ts
+++ src/data/datetime.ts
@@ -1,5 +1,5 @@
 export function readDateTime(buffer: Buffer): Date {
     const td = buffer.readDoubleLE();
     const daysDiff = 25569; // days between 1899-12-30 and 01-01-19070
-    return new Date((td - daysDiff) * 86400 * 1000);
+    return new Date(Math.round((td - daysDiff) * 86400 * 1000));
 }
