f235131b4ba1c1bdce800f776ee584457af4b0c8
Andi Pätzold <github@andipaetzold.com>
2022-01-07 00:32:37 +0100

    refactor: simplify MDBReader export in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
-import MDBReader from "./MDBReader";
-export default MDBReader;
-
+export { default } from "./MDBReader";
 export type { Column, ColumnType } from "./column";
 export type { Value } from "./data";
 export type { default as Table } from "./Table";
