13b05d515b882a185747b14667783ccb98d016d5
Andi Pätzold <github@andipaetzold.com>
2022-01-08 14:52:54 +0100

    test: `getPassword` for Jet 3

--- test/password/password.spec.ts
+++ test/password/password.spec.ts
@@ -2,10 +2,11 @@ import { resolve } from "path";
 import { readFileSync } from "fs";
 import MDBReader from "../../src";
 
-// TODO: test Jet 3
+// TODO: test Jet 3 with password
 // TODO: add V2010_with-password.accdb once decryption works
 describe.each`
     filename                     | password
+    ${"V1997_no-password.mdb"}   | ${null}
     ${"V2000_with-password.mdb"} | ${"password"}
     ${"V2000_no-password.mdb"}   | ${null}
     ${"V2003_with-password.mdb"} | ${"password"}
