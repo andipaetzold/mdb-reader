cad4eb45c8303b65f29b41aeec011e455e55709e
Andi Pätzold <github@andipaetzold.com>
2022-01-13 23:51:13 +0100

    refactor: shorten hash algorithm constants

--- src/PageDecrypter/office/HashAlgorithm.ts
+++ src/PageDecrypter/office/HashAlgorithm.ts
@@ -2,10 +2,7 @@ export interface HashAlgorithm {
     id: number;
 }
 
-const HASH_ALGORITHM_EXTERNAL: HashAlgorithm = { id: 0 };
-const HASH_ALGORITHM_SHA1: HashAlgorithm = { id: 0x8004 };
+const EXTERNAL: HashAlgorithm = { id: 0 };
+const SHA1: HashAlgorithm = { id: 0x8004 };
 
-export const HASH_ALGORITHMS = {
-    EXTERNAL: HASH_ALGORITHM_EXTERNAL,
-    SHA1: HASH_ALGORITHM_SHA1,
-};
+export const HASH_ALGORITHMS = { EXTERNAL, SHA1 };
