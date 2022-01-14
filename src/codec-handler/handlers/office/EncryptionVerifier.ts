59dd95acd8d71280f3f39ac44a2e897d85ec2ff8
Andi Pätzold <github@andipaetzold.com>
2022-01-14 20:08:00 +0100

    feat: verify rc4 crypto api password

--- src/codec-handler/handlers/office/EncryptionVerifier.ts
+++ src/codec-handler/handlers/office/EncryptionVerifier.ts
@@ -4,14 +4,18 @@ import { CryptoAlgorithm } from "./CryptoAlgorithm";
 const SALT_SIZE_OFFSET = 138;
 const SALT_OFFSET = 142;
 
-// const ENC_VERIFIER_SIZE = 16;
+const ENC_VERIFIER_SIZE = 16;
 const SALT_SIZE = 16;
 
 export interface EncryptionVerifier {
     readonly salt: Buffer;
+    readonly encryptionVerifier: Buffer;
+
+    readonly encryptionVerifierHashSize: number;
+    readonly encryptionVerifierHash: Buffer;
 }
 
-export function parseEncryptionVerifier(encryptionProvider: Buffer, _cryptoAlgorithm: CryptoAlgorithm): EncryptionVerifier {
+export function parseEncryptionVerifier(encryptionProvider: Buffer, cryptoAlgorithm: CryptoAlgorithm): EncryptionVerifier {
     const saltSize = encryptionProvider.readInt32LE(SALT_SIZE_OFFSET);
 
     if (saltSize !== SALT_SIZE) {
@@ -20,16 +24,16 @@ export function parseEncryptionVerifier(encryptionProvider: Buffer, _cryptoAlgor
 
     const salt = encryptionProvider.slice(SALT_OFFSET, SALT_OFFSET + SALT_SIZE);
 
-    // const encryptionVerifierOffset = SALT_OFFSET + SALT_SIZE;
-    // const verifierHashSizeOffset = encryptionVerifierOffset + ENC_VERIFIER_SIZE;
-    // const verifierHashOffset = verifierHashSizeOffset + 4;
+    const encryptionVerifierOffset = SALT_OFFSET + SALT_SIZE;
+    const verifierHashSizeOffset = encryptionVerifierOffset + ENC_VERIFIER_SIZE;
+    const verifierHashOffset = verifierHashSizeOffset + 4;
 
-    // const encryptionVerifier = encryptionProvider.slice(encryptionVerifierOffset, verifierHashSizeOffset);
-    // const verifierHashSize = encryptionProvider.readInt32LE(verifierHashSizeOffset);
-    // const verifierHash = encryptionProvider.slice(
-    //     verifierHashOffset,
-    //     verifierHashOffset + cryptoAlgorithm.encryptionVerifierHashLength
-    // );
+    const encryptionVerifier = encryptionProvider.slice(encryptionVerifierOffset, verifierHashSizeOffset);
+    const encryptionVerifierHashSize = encryptionProvider.readInt32LE(verifierHashSizeOffset);
+    const encryptionVerifierHash = encryptionProvider.slice(
+        verifierHashOffset,
+        verifierHashOffset + cryptoAlgorithm.encryptionVerifierHashLength
+    );
 
-    return { salt };
+    return { salt, encryptionVerifier, encryptionVerifierHash, encryptionVerifierHashSize };
 }
--- src/codec-handler/handlers/office/rc4-cryptoapi.ts
+++ src/codec-handler/handlers/office/rc4-cryptoapi.ts
@@ -1,10 +1,9 @@
-import { CodecHandler } from "../..";
 import { decryptRC4, hash } from "../../../crypto";
-import { roundToFullByte } from "../../../util";
-import { DecryptPage } from "../../types";
+import { fixBufferLength, intToBuffer, roundToFullByte } from "../../../util";
+import { CodecHandler, DecryptPage } from "../../types";
 import { getPageEncodingKey } from "../../util";
 import { CryptoAlgorithm, CRYPTO_ALGORITHMS } from "./CryptoAlgorithm";
-import { parseEncryptionHeader } from "./EncryptionHeader";
+import { EncryptionHeader, parseEncryptionHeader } from "./EncryptionHeader";
 import { parseEncryptionVerifier } from "./EncryptionVerifier";
 import { HashAlgorithm, HASH_ALGORITHMS } from "./HashAlgorithm";
 
@@ -19,22 +18,41 @@ export function createRC4CryptoAPICodecHandler(
     const headerLength = encryptionProvider.readInt32LE(8);
     const headerBuffer = encryptionProvider.slice(12, 12 + headerLength);
 
-    const header = parseEncryptionHeader(headerBuffer, VALID_CRYPTO_ALGORITHMS, VALID_HASH_ALGORITHMS);
+    const encryptionHeader = parseEncryptionHeader(headerBuffer, VALID_CRYPTO_ALGORITHMS, VALID_HASH_ALGORITHMS);
+    const encryptionVerifier = parseEncryptionVerifier(encryptionProvider, encryptionHeader.cryptoAlgorithm);
 
-    const verifier = parseEncryptionVerifier(encryptionProvider, header.cryptoAlgorithm);
-
-    const baseHash = hash("sha1", [verifier.salt, password]);
+    const baseHash = hash("sha1", [encryptionVerifier.salt, password]);
 
     const decryptPage: DecryptPage = (pageBuffer, pageIndex) => {
         const pageEncodingKey = getPageEncodingKey(encodingKey, pageIndex);
-        let key = hash("sha1", [baseHash, pageEncodingKey], roundToFullByte(header.keySize));
-        if (header.keySize === 40) {
-            key = key.slice(0, roundToFullByte(128));
-        }
-        return decryptRC4(key, pageBuffer);
+        const encryptionKey = getEncryptionKey(encryptionHeader, baseHash, pageEncodingKey);
+        return decryptRC4(encryptionKey, pageBuffer);
     };
     return {
         decryptPage,
-        verifyPassword: () => true,
+        verifyPassword: () => {
+            const encryptionKey = getEncryptionKey(encryptionHeader, baseHash, intToBuffer(0));
+            const decrypted = decryptRC4(
+                encryptionKey,
+                Buffer.concat([encryptionVerifier.encryptionVerifier, encryptionVerifier.encryptionVerifierHash])
+            );
+            const verifier = decrypted.slice(0, encryptionVerifier.encryptionVerifier.length);
+            const verifierHash = fixBufferLength(
+                decrypted.slice(encryptionVerifier.encryptionVerifier.length),
+                encryptionVerifier.encryptionVerifierHashSize
+            );
+
+            const testHash = fixBufferLength(hash("sha1", [verifier]), encryptionVerifier.encryptionVerifierHashSize);
+
+            return verifierHash.equals(testHash);
+        },
     };
+}
+
+function getEncryptionKey(header: EncryptionHeader, baseHash: Buffer, data: Buffer): Buffer {
+    const key = hash("sha1", [baseHash, data], roundToFullByte(header.keySize));
+    if (header.keySize === 40) {
+        return key.slice(0, roundToFullByte(128));
+    }
+    return key;
 }
