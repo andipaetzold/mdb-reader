b2d143e0276692f0deea08c47ef583c0b66b1904
Andi Pätzold <github@andipaetzold.com>
2022-02-22 23:08:05 +0100

    fix: handle non-ascii characters in Jet3 databases
    close #172

--- src/unicodeCompression.ts
+++ src/unicodeCompression.ts
@@ -3,9 +3,11 @@ import { JetFormat } from "./JetFormat";
 /**
  * @see https://github.com/brianb/mdbtools/blob/d6f5745d949f37db969d5f424e69b54f0da60b9b/HACKING#L823-L831
  */
-export function uncompressText(buffer: Buffer, format: Pick<JetFormat, 'textEncoding'>): string {
-    if (format.textEncoding === "utf8") {
-        return buffer.toString("utf8");
+export function uncompressText(buffer: Buffer, format: Pick<JetFormat, "textEncoding">): string {
+    if (format.textEncoding === "unknown") {
+        // We assume windows 1252 (windows default) is used
+        // In some cases this might not work
+        return decodeWindows1252(buffer);
     }
 
     if (buffer.length <= 2 || (buffer.readUInt8(0) & 0xff) !== 0xff || (buffer.readUInt8(1) & 0xff) !== 0xfe) {
@@ -32,4 +34,38 @@ export function uncompressText(buffer: Buffer, format: Pick<JetFormat, 'textEnco
     }
 
     return uncompressedBuffer.slice(0, uncompressedBufferPos).toString("ucs-2");
+}
+
+/**
+ * @see https://github.com/ashtuchkin/iconv-lite/blob/928f7c68e1be51c1391c70dbee244fd32623f121/encodings/sbcs-codec.js#L17-L19
+ */
+const ASCII_CHARS = Array.from(new Array(128).keys())
+    .map((i) => String.fromCharCode(i))
+    .join("");
+/**
+ * @see https://github.com/ashtuchkin/iconv-lite/blob/5d99a923f2bb9352abf80f8aeb850d924a8a1e38/encodings/sbcs-data-generated.js#L82
+ */
+const WINDOWS_1252_CHARS =
+    "€�‚ƒ„…†‡ˆ‰Š‹Œ�Ž��‘’“”•–—˜™š›œ�žŸ ¡¢£¤¥¦§¨©ª«¬­®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿ";
+
+/**
+ * Decodes CP1252 / windows 1252
+ *
+ * Copied in to not add a dependency and keep the bundle (browser) small
+ *
+ * @see https://github.com/ashtuchkin/iconv-lite/blob/928f7c68e1be51c1391c70dbee244fd32623f121/encodings/sbcs-codec.js#L58-L69
+ */
+function decodeWindows1252(buffer: Buffer): string {
+    const chars = `${ASCII_CHARS}${WINDOWS_1252_CHARS}`;
+    const charsBuffer = Buffer.from(chars, "ucs2");
+
+    const result = Buffer.alloc(buffer.length * 2);
+
+    for (let i = 0; i < buffer.length; ++i) {
+        const idx1 = buffer[i] * 2;
+        result[i * 2] = charsBuffer[idx1];
+        result[i * 2 + 1] = charsBuffer[idx1 + 1];
+    }
+
+    return result.toString("ucs2");
 }
