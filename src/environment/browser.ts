44859521b308e3b7c8a77a3f0c5a03092fad5462
Andi Pätzold <github@andipaetzold.com>
2022-02-26 15:22:02 +0100

    fix: use default import for browserify-aes

--- src/environment/browser.ts
+++ src/environment/browser.ts
@@ -1,3 +1,3 @@
-export { createDecipheriv } from "browserify-aes/browser.js";
+import browserifyAES from "browserify-aes/browser.js";
+export const createDecipheriv = browserifyAES.createDecipheriv;
 export { default as createHash } from "create-hash";
-
