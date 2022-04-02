8f2d9f319f22fbe3d000cbd9bc38d3a3dc6ead59
Andi Pätzold <github@andipaetzold.com>
2022-04-02 10:40:44 +0200

    docs: add `to-json` example

---
+++ examples/to-json/src/index.mjs
@@ -0,0 +1,20 @@
+import MDBReader from "mdb-reader";
+import fs from "fs";
+
+const filename = process.argv[2];
+
+if (!filename || !fs.existsSync(filename)) {
+    console.error("Missing filename");
+    process.exit(1);
+}
+
+const buffer = fs.readFileSync(filename);
+
+const reader = new MDBReader(buffer);
+const tableNames = reader.getTableNames();
+
+const data = {};
+for (const tableName of tableNames) {
+    data[tableName] = reader.getTable(tableName).getData();
+}
+console.log(JSON.stringify(data));
