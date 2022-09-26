f865ac1e327100a3775a0d6d8765d3f8b3d42956
Andi Pätzold <github@andipaetzold.com>
2022-09-27 00:31:03 +0200

    refactor: remove unused method blockEncrypt

--- src/crypto/blockDecrypt.ts
+++ src/crypto/blockDecrypt.ts
@@ -7,10 +7,3 @@ export function blockDecrypt(cipher: Cipher, key: Buffer, iv: Buffer, data: Buff
     decipher.setAutoPadding(false);
     return decipher.update(data);
 }
-
-export function blockEncrypt(cipher: Cipher, key: Buffer, iv: Buffer, data: Buffer): Buffer {
-    const algorithm = `${cipher.algorithm}-${key.length * 8}-${cipher.chaining.slice(-3)}`;
-    const decipher = createDecipheriv(algorithm, key, iv);
-    decipher.setAutoPadding(false);
-    return decipher.update(data);
-}
