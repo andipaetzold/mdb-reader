7149b33aaa51552c60da952f9726398d1dc73a99
Andi Pätzold <github@andipaetzold.com>
2023-05-02 02:31:54 +0200

    chore: support node 20 (#279)
    * chore: support node 20
    
    * test: initialize reader within actual tests
    
    * test: increase timeout

--- test/ole.spec.ts
+++ test/ole.spec.ts
@@ -1,18 +1,18 @@
 import { readFileSync } from "fs";
-import MDBReader, { Table } from "../src/index.js";
+import MDBReader from "../src/index.js";
 import { resolve } from "path";
 
 describe("OLE", () => {
-    let table: Table;
+    let buffer: Buffer;
 
     beforeEach(() => {
         const path = resolve("test/data", "V2007", "ole.accdb");
-        const buffer = readFileSync(path);
-        const reader = new MDBReader(buffer);
-        table = reader.getTable("Table1");
+        buffer = readFileSync(path);
     });
 
     it("reads ole data ", () => {
+        const reader = new MDBReader(buffer);
+        const table = reader.getTable("Table1");
         table.getData();
         // TODO: check for correct values
     });
