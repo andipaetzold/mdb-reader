9d7caaa2649dfd309d437f8f21510cf45807c066
Andi Pätzold <github@andipaetzold.com>
2023-06-08 11:31:46 +0200

    test: add test

---
+++ test/longtext.spec.ts
@@ -0,0 +1,15 @@
+import { expect } from "chai";
+import { readFileSync } from "fs";
+import { it } from "mocha";
+import { resolve } from "path";
+import MDBReader from "../src/index.js";
+
+describe("LongText", () => {
+    it("multiple pages", () => {
+        const path = resolve("test/data/V2016/longtext.accdb");
+        const buffer = readFileSync(path);
+        const reader = new MDBReader(buffer);
+        const data = reader.getTable("Table1").getData();
+        expect(data[0].LongText).to.have.length(5000);
+    });
+});
