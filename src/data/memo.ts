c91819fada0754b0d4ddb2da62f05ee4db5a269a
Andi Pätzold <github@andipaetzold.com>
2023-06-08 11:33:56 +0200

    refactor: use switch-case for memo types

--- src/data/memo.ts
+++ src/data/memo.ts
@@ -2,43 +2,50 @@ import { Column } from "../index.js";
 import Database from "../Database.js";
 import { uncompressText } from "../unicodeCompression.js";
 
+const TYPE_THIS_PAGE = 0x80;
+const TYPE_OTHER_PAGE = 0x40;
+const TYPE_OTHER_PAGES = 0x00;
+
 /**
  * @see https://github.com/brianb/mdbtools/blob/d6f5745d949f37db969d5f424e69b54f0da60b9b/src/libmdb/data.c#L690-L776
  */
 export function readMemo(buffer: Buffer, _col: Column, db: Database): string {
     const memoLength = buffer.readUIntLE(0, 3);
 
-    const bitmask = buffer.readUInt8(3);
-    if (bitmask & 0x80) {
-        // inline
-        return uncompressText(buffer.slice(12, 12 + memoLength), db.format);
-    } else if (bitmask & 0x40) {
-        // single page
-        const pageRow = buffer.readUInt32LE(4);
-        const rowBuffer = db.findPageRow(pageRow);
-        return uncompressText(rowBuffer.slice(0, memoLength), db.format);
-    } else if (bitmask === 0) {
-        // multi page
-        let pageRow = buffer.readInt32LE(4);
-        let memoDataBuffer = Buffer.alloc(0);
-        do {
+    const type = buffer.readUInt8(3);
+    switch (type) {
+        case TYPE_THIS_PAGE: {
+            return uncompressText(buffer.slice(12, 12 + memoLength), db.format);
+        }
+
+        case TYPE_OTHER_PAGE: {
+            const pageRow = buffer.readUInt32LE(4);
             const rowBuffer = db.findPageRow(pageRow);
+            return uncompressText(rowBuffer.slice(0, memoLength), db.format);
+        }
 
-            if (memoDataBuffer.length + rowBuffer.length - 4 > memoLength) {
-                break;
-            }
+        case TYPE_OTHER_PAGES: {
+            let pageRow = buffer.readInt32LE(4);
+            let memoDataBuffer = Buffer.alloc(0);
+            do {
+                const rowBuffer = db.findPageRow(pageRow);
 
-            if (rowBuffer.length === 0) {
-                break;
-            }
+                if (memoDataBuffer.length + rowBuffer.length - 4 > memoLength) {
+                    break;
+                }
 
-            memoDataBuffer = Buffer.concat([memoDataBuffer, rowBuffer.slice(4)]);
+                if (rowBuffer.length === 0) {
+                    break;
+                }
 
-            pageRow = rowBuffer.readInt32LE(0);
-        } while (pageRow !== 0);
+                memoDataBuffer = Buffer.concat([memoDataBuffer, rowBuffer.slice(4)]);
 
-        return uncompressText(memoDataBuffer.slice(0, memoLength), db.format);
-    } else {
-        throw new Error(`Unknown memo type ${bitmask}`);
+                pageRow = rowBuffer.readInt32LE(0);
+            } while (pageRow !== 0);
+
+            return uncompressText(memoDataBuffer.slice(0, memoLength), db.format);
+        }
+        default:
+            throw new Error(`Unknown memo type ${type}`);
     }
 }
