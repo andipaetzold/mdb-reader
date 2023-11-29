d04896fd7ca720302a2fd6c6592aa28c31f15af7
Oliver Dowling <oliver.dowling@gmail.com>
2023-11-29 18:29:16 +1100

    Upgrade SK example to use mdb-reader v3 and polyfills

--- examples/sveltekit/src/lib/database.ts
+++ examples/sveltekit/src/lib/database.ts
@@ -1,7 +1,4 @@
-import { Buffer } from 'buffer';
 import MDBReader from 'mdb-reader';
-
-globalThis.Buffer = Buffer;
 
 export async function getTableNames(file: File) {
 	const buffer = Buffer.from(await file.arrayBuffer());
--- examples/sveltekit/vite.config.ts
+++ examples/sveltekit/vite.config.ts
@@ -1,12 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	resolve: {
-		alias: {
-			buffer: 'buffer/',
-			crypto: 'browserify-aes'
-		}
-	}
+	plugins: [
+		nodePolyfills({
+			include: ['buffer', 'stream', 'util'],
+			globals: { Buffer: true }
+		}),
+		sveltekit()
+	]
 });
