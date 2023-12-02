7aa0591ccd84745e59aaf00b2942481c0fdf5d7b
Andi Pätzold <github@andipaetzold.com>
2023-12-02 02:40:13 +0100

    refactor: make CodecHandler fully async

--- src/codec-handler/types.ts
+++ src/codec-handler/types.ts
@@ -3,5 +3,5 @@ export interface CodecHandler {
     verifyPassword: VerifyPassword;
 }
 
-export type DecryptPage = (pageBuffer: Buffer, pageIndex: number) => Promise<Buffer> | Buffer;
-export type VerifyPassword = () => Promise<boolean> | boolean;
+export type DecryptPage = (pageBuffer: Buffer, pageIndex: number) => Promise<Buffer>;
+export type VerifyPassword = () => Promise<boolean>;
