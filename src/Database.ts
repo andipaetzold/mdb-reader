2b721c2cea8ef74898b45e2f2357b763e42c0a56
Andi Pätzold <github@andipaetzold.com>
2023-12-02 02:41:39 +0100

    refactor: make `findRow` private

--- src/Database.ts
+++ src/Database.ts
@@ -130,7 +130,7 @@ export class Database {
         const row = pageRow & 0xff;
 
         const pageBuffer = await this.getPage(page);
-        return this.findRow(pageBuffer, row);
+        return this.#findRow(pageBuffer, row);
     }
 
     /**
@@ -138,7 +138,7 @@ export class Database {
      *
      * @see https://github.com/brianb/mdbtools/blob/d6f5745d949f37db969d5f424e69b54f0da60b9b/src/libmdb/data.c#L126-L138
      */
-    findRow(pageBuffer: Buffer, row: number): Buffer {
+    #findRow(pageBuffer: Buffer, row: number): Buffer {
         const rco = this.#format.dataPage.recordCountOffset;
 
         if (row > 1000) {
