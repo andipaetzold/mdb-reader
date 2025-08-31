0811ab7986dcf5be50839cb237a587b01775ac12
Andi Pätzold <github@andipaetzold.com>
2025-08-31 16:52:51 +0200

    refactor: type and structure tweaks

--- examples/sveltekit/src/routes/+page.server.ts
+++ examples/sveltekit/src/routes/+page.server.ts
@@ -1,21 +1,22 @@
-import { getTableNames } from '$lib/database';
+import { getTableNames } from "$lib/database";
+import type { Actions } from "@sveltejs/kit";
 
 export const actions = {
-	default: async ({ request }) => {
-		const formData = await request.formData();
-		const files = formData.getAll('files').map(async (file) => {
-			if (file instanceof File) {
-				return {
-					name: file.name,
-					tableNames: await getTableNames(file)
-				};
-			} else {
-				return {
-					name: file,
-					tableNames: []
-				};
-			}
-		});
-		return { files: await Promise.all(files) };
-	}
-};
+    default: async ({ request }) => {
+        const formData = await request.formData();
+        const files = formData.getAll("files").map(async (file) => {
+            if (file instanceof File) {
+                return {
+                    name: file.name,
+                    tableNames: await getTableNames(file),
+                };
+            } else {
+                return {
+                    name: file,
+                    tableNames: [],
+                };
+            }
+        });
+        return { files: await Promise.all(files) };
+    },
+} satisfies Actions;
--- examples/sveltekit/src/routes/+page.svelte
+++ examples/sveltekit/src/routes/+page.svelte
@@ -1,44 +1,44 @@
 <script lang="ts">
-	import { getTableNames } from '$lib/database';
+    import { getTableNames } from "$lib/server/database.js";
 
-	let { form } = $props();
+    let { form } = $props();
 
-	let files: FileList | undefined = $state();
+    let files: FileList | undefined = $state();
 </script>
 
 <form method="post" enctype="multipart/form-data">
-	<input name="files" type="file" bind:files multiple accept=".mdb,.accdb" />
-	<button type="submit">Upload</button>
+    <input name="files" type="file" bind:files multiple accept=".mdb,.accdb" />
+    <button type="submit">Upload</button>
 </form>
 
 {#if form?.files}
-	<h2>Uploaded Files:</h2>
-	{#each form.files as file}
-		<section>
-			{file.name}
-			{#if file.tableNames.length}
-				<ul>
-					{#each file.tableNames as tableName}
-						<li>{tableName}</li>
-					{/each}
-				</ul>
-			{/if}
-		</section>
-	{/each}
+    <h2>Uploaded Files:</h2>
+    {#each form.files as file}
+        <section>
+            {file.name}
+            {#if file.tableNames.length}
+                <ul>
+                    {#each file.tableNames as tableName}
+                        <li>{tableName}</li>
+                    {/each}
+                </ul>
+            {/if}
+        </section>
+    {/each}
 {/if}
 
 {#if files}
-	<h2>Selected Files:</h2>
-	{#each files as file}
-		<section>
-			{file.name}:
-			{#await getTableNames(file) then tableNames}
-				<ul>
-					{#each tableNames as tableName}
-						<li>{tableName}</li>
-					{/each}
-				</ul>
-			{/await}
-		</section>
-	{/each}
+    <h2>Selected Files:</h2>
+    {#each files as file}
+        <section>
+            {file.name}:
+            {#await getTableNames(file) then tableNames}
+                <ul>
+                    {#each tableNames as tableName}
+                        <li>{tableName}</li>
+                    {/each}
+                </ul>
+            {/await}
+        </section>
+    {/each}
 {/if}
