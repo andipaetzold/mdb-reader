57be2737619084e80b3bcffb08c63b953422cbfa
Andi Pätzold <github@andipaetzold.com>
2026-02-18 00:56:17 +0100

    refactor: improve readability in `readOLE`

--- src/data/ole.ts
+++ src/data/ole.ts
@@ -1,45 +1,66 @@
 import type { Column } from "../column.js";
 import { Database } from "../Database.js";
 
+const TYPES = {
+    THIS_PAGE: 0x80,
+    OTHER_PAGE: 0x40,
+    OTHER_PAGES: 0x00,
+} as const;
+
 /**
  * @see https://github.com/brianb/mdbtools/blob/d6f5745d949f37db969d5f424e69b54f0da60b9b/src/libmdb/data.c#L626-L688
+ * @see https://github.com/spannm/jackcess/blob/4c433a4ae4969ff5658556806a35ccb13cc35313/src/main/java/io/github/spannm/jackcess/impl/LongValueColumnImpl.java#L130-L209
  */
 export function readOLE(buffer: Buffer, _col: Column, database: Database): Buffer {
-    const memoLength = buffer.readUIntLE(0, 3);
+    const length = buffer.readUIntLE(0, 3);
+    const type = buffer.readUInt8(3);
 
-    const bitmask = buffer.readUInt8(3);
+    switch (type) {
+        case TYPES.THIS_PAGE: {
+            // 0-2: Length
+            // 3: Type
+            // 4-7: lval_dp
+            // 8-11: unknown
+            // 12: data
 
-    if (bitmask & 0x80) {
-        // inline
-        return buffer.slice(12, 12 + memoLength);
-    } else if (bitmask & 0x40) {
-        // single page
-        const pageRow = buffer.readUInt32LE(4);
-        const rowBuffer = database.findPageRow(pageRow);
-        return rowBuffer.slice(0, memoLength);
-    } else if (bitmask === 0) {
-        // multi page
-        let pageRow = buffer.readInt32LE(4);
-
-        let memoDataBuffer = Buffer.alloc(0);
-        do {
+            // inline
+            return buffer.slice(12, 12 + length);
+        }
+        case TYPES.OTHER_PAGE: {
+            // single page
+            const pageRow = buffer.readUInt32LE(4);
             const rowBuffer = database.findPageRow(pageRow);
+            return rowBuffer.slice(0, length);
+        }
+        case TYPES.OTHER_PAGES: {
+            // multi page
+            let pageRow = buffer.readInt32LE(4);
+            
+            const result = Buffer.alloc(length);
+            
+            let offset = 0;
+            do {
+                const rowBuffer = database.findPageRow(pageRow);
 
-            if (memoDataBuffer.length + rowBuffer.length - 4 > memoLength) {
-                break;
-            }
+                if (result.length + rowBuffer.length - 4 > length) {
+                    break;
+                }
 
-            if (rowBuffer.length === 0) {
-                break;
-            }
+                if (rowBuffer.length === 0) {
+                    break;
+                }
 
-            memoDataBuffer = Buffer.concat([memoDataBuffer, rowBuffer.slice(4, buffer.length)]);
+                pageRow = rowBuffer.readUInt32LE(0);
 
-            pageRow = rowBuffer.readUInt32LE(0);
-        } while (pageRow !== 0);
+                const newChunk = rowBuffer.slice(4, rowBuffer.length);
+                newChunk.copy(result, offset);
+                offset += newChunk.length;
+            } while (pageRow !== 0);
 
-        return memoDataBuffer.slice(0, memoLength);
-    } else {
-        throw new Error(`Unknown memo type ${bitmask}`);
+            return result.subarray(0, length);
+        }
+        default: {
+            throw new Error(`Unknown OLE type ${type}`);
+        }
     }
 }
